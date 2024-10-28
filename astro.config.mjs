71afe80584177611d271eae774af4a98efd4588c
mssnzz <76191988+mssnzz@users.noreply.github.com>
2024-10-28 12:28:16 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     server: {
-        port: 4321, // Cambia a tu puerto preferido
+        port: 80, // Cambia a tu puerto preferido
     },
 });
 
